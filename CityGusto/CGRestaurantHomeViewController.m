8dd65b58ba106ce496f7afff58d7a04634c61663
Padraic Doyle <padraic@givefit.com>
2013-03-30 20:32:59 -0400

    styleing

--- CityGusto/CGRestaurantHomeViewController.m
+++ CityGusto/CGRestaurantHomeViewController.m
@@ -52,6 +52,10 @@
     [topFiveView.layer setBorderColor:[UIColor lightGrayColor].CGColor];
     [topFiveView.layer setBorderWidth:0.5f];
     
+    [ratingsView.layer setCornerRadius:5.0f];
+    [ratingsView.layer setBorderColor:[UIColor lightGrayColor].CGColor];
+    [ratingsView.layer setBorderWidth:0.5f];
+    
     self.addressLabel.text = self.restaurant.address1;
     NSString *cityText = self.restaurant.cityName;
     cityText = [cityText stringByAppendingString:@", "];
